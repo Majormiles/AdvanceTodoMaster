f13443c0b05308c0da3d9685a49f257b56770f49
Majormyles <majormilesvigour@gmail.com>
2025-06-05 08:43:48 -1200

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
